ee653b54423cd015eef455e0e369705b59e63bd1
Дмитрий Дуля <dmitrodulja@ukr.net>
2020-04-05 08:07:54 +0300

    add logic for file information(FileInformation class); test it;

---
+++ HurtomFiles.Logic/Title.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HurtomFiles.Logic
+{
+    public readonly struct Title
+    {
+        // TODO: Parse title
+        public readonly string fullTitle;
+        public readonly string origTitle;
+        public readonly string ukrTitle;
+        public readonly int year;
+        public readonly string information;
+
+        public Title(string titleString)
+        {
+            this.fullTitle = titleString;
+            origTitle = ukrTitle = information = "";
+            year = 0;
+        }
+
+        public override string ToString() => fullTitle;
+    }
+}
